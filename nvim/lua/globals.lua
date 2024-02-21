31779ec9b60b7ec933c88c2254b5aca4011c54fd
Ramy Fakhoury <Ramy.fakhoury@vastdata.com>
2024-02-21 14:07:30 +0200

    :wink

--- nvim/lua/globals.lua
+++
@@ -1,8 +0,0 @@
--- Common options we'll pass to vim.keymap.set() method
-NOREMAP = function(desc)
-  return { noremap = true, desc = desc }
-end
-
-NOREMAP_SILENT = function(desc)
-  return { noremap = true, silent = true, desc = desc }
-end
